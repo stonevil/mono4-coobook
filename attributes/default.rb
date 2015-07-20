6f0d70b72d701764381b60f70eeda679453b71d3
Alex Slynko <alex.slynko@wonga.com>
2015-07-20 18:16:12 +0100

    Added simple kitchen tests, support for old versions of mono and ubuntu 12.04 support

--- attributes/default.rb
+++ attributes/default.rb
@@ -26,8 +26,8 @@ when 'debian'
   default['mono4']['apt_repository'] = 'http://download.mono-project.com/repo/debian'
   default['mono4']['apt_keyserver'] = 'keyserver.ubuntu.com'
   default['mono4']['apt_repository_key'] = '3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF'
+  default['mono4']['apt_distribution'] = 'wheezy'
   default['mono4']['package_name'] = 'mono-complete'
-  default['mono4']['package_version'] = '4.0.1-0xamarin5'
 
   default['mono4']['source']['dependencies'] = %w(
     makedev
